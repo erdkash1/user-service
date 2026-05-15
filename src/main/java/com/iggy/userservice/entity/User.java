bd80c19f1e6bbbe7940b29c5d8a8b9703aded304
Erdkash1 <erdkash1@gmail.com>
2026-05-14 23:56:40 -0500

    Creating classes

---
+++ src/main/java/com/iggy/userservice/entity/User.java
@@ -0,0 +1,36 @@
+package com.iggy.userservice.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
