ceceb15566eccb954fd84dcaaaab9974ad8cdc84
Erdkash1 <erdkash1@gmail.com>
2026-05-14 23:57:00 -0500

    Creating classes

--- src/main/java/com/iggy/userservice/UserServiceApplication.java
+++ src/main/java/com/iggy/userservice/UserServiceApplication.java
@@ -1,4 +1,4 @@
-package com.lggy.userservice;
+package com.iggy.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
